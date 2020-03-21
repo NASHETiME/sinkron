7dfc5c18d6519e73ad466e6dd7651490889a0133
George Guvamatanga <32019551+NASHETiME@users.noreply.github.com>
2020-03-21 17:54:02 +0200

    Update eclient.py

--- eclient.py
+++ eclient.py
@@ -1,88 +1,110 @@
 from tkinter import *
+from functools import partial
 import time
 import mouse
 import socket
 import re
 from pynput import keyboard
-
-created_window = False
-made = False
-
-root = Tk()
-
-root.attributes('-alpha', 0.01)
-root.attributes('-fullscreen', True)
-root.config(cursor='none')
-
-HOST = '192.168.137.35'
-PORT = 6546
+import threading
 
 
+def connect(ip, port):
+	created_window = False
+	made = False
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	s.connect((HOST, PORT))
+	root = Tk()
 
-	
+	root.attributes('-alpha', 0.01)
+	root.attributes('-fullscreen', True)
+	root.config(cursor='none')
 
-	events = []
-	mouse.hook(events.append)
+	HOST = ip
+	PORT = int(port)
 
-	def on_press(key):
-		if created_window:
-			try:
-				s.sendall(('kbpa:' + key.char).encode('utf-8'))
-			except AttributeError:
-				s.sendall(('kbps:' + str(key)).encode('utf-8'))
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.connect((HOST, PORT))
 
-	def on_release(key):
-		if created_window:
-			try:
-				s.sendall(('kbra:' + key.char).encode('utf-8'))
-			except AttributeError:
-				s.sendall(('kbrs:' + str(key)).encode('utf-8'))
-		if key == keyboard.Key.esc:
-			# Stop listener
-			return False
+		events = []
+		mouse.hook(events.append)
 
-	# ...or, in a non-blocking fashion:
-	listener = keyboard.Listener(
-		on_press=on_press,
-		on_release=on_release)
-	listener.start()
-
-	while 1:
-		mouse._listener.queue.join()
-		for event in events:
-
-			# etype = str(type(event))[re.search('\'.+\'', str(type(event))).span()[0]:re.search('\'.+\'', str(type(event))).span()[1]].split('.')[-1][:-1]
-			print(event)
-
+		def on_press(key):
 			if created_window:
-				root.update()
-				s.sendall((str(event)).encode('utf-8'))
+				try:
+					s.sendall(('kbpa:' + key.char).encode('utf-8'))
+				except AttributeError:
+					s.sendall(('kbps:' + str(key)).encode('utf-8'))
 
-			try:
-				if event.x > 1022 and not created_window and not made:
-					created_window = True
-					made = True
+		def on_release(key):
+			if created_window:
+				try:
+					s.sendall(('kbra:' + key.char).encode('utf-8'))
+				except AttributeError:
+					s.sendall(('kbrs:' + str(key)).encode('utf-8'))
+			if key == keyboard.Key.esc:
+				# Stop listener
+				return False
+
+		# ...or, in a non-blocking fashion:
+		listener = keyboard.Listener(
+			on_press=on_press,
+			on_release=on_release)
+		listener.start()
+
+		while 1:
+			mouse._listener.queue.join()
+			for event in events:
+
+				# etype = str(type(event))[re.search('\'.+\'', str(type(event))).span()[0]:re.search('\'.+\'', str(type(event))).span()[1]].split('.')[-1][:-1]
+				print(event)
+
+				if created_window:
 					root.update()
-					mouse.move(100, mouse.get_position()[1])
-				elif event.x > 1022 and made and not created_window:
-					root = Tk()
+					s.sendall((str(event)).encode('utf-8'))
 
-					root.attributes('-alpha', 0.01)
-					root.attributes('-fullscreen', True)
-					root.config(cursor='none')
-					created_window = True
-					root.update()
-					mouse.move(100, mouse.get_position()[1])
-				elif event.x < 1 and created_window:
-					created_window = False
-					root.destroy()
-					mouse.move(1300, mouse.get_position()[1])
-			except:
-				pass
+				try:
+					if event.x > (screen_dimensions[0] - 5) and not created_window and not made:
+						created_window = True
+						made = True
+						root.update()
+						mouse.move(100, mouse.get_position()[1])
+					elif event.x > (screen_dimensions[0] - 5) and made and not created_window:
+						root = Tk()
 
-		del events[:]
-	
-print('Received', repr(data))
+						root.attributes('-alpha', 0.01)
+						root.attributes('-fullscreen', True)
+						root.config(cursor='none')
+						created_window = True
+						root.update()
+						mouse.move(100, mouse.get_position()[1])
+					elif event.x < 1 and created_window:
+						created_window = False
+						root.destroy()
+						mouse.move(1300, mouse.get_position()[1])
+				except:
+					pass
+
+			del events[:]
+		
+def connect_thread(ip, port):
+	connect_thread = threading.Thread(target=connect, args=(ip.get(), port.get()))
+	connect_thread.start()
+
+tk_window = Tk()
+tk_window.geometry('200x150')
+tk_window.title('Sinkron')
+
+ip_label = Label(tk_window, text="IP Adress").grid(row=0, column=0)
+ip = StringVar()
+ip_entry = Entry(tk_window, textvariable=ip).grid(row=0, column=1)
+
+port_label = Label(tk_window, text="Port").grid(row=1, column=0)
+port = StringVar()
+port_entry = Entry(tk_window, textvariable=port).grid(row=1, column=1)
+
+connect_thread = partial(connect_thread, ip, port)
+
+connect_button = Button(tk_window, text="Connect", command=connect_thread).grid(row=4, column=0)
+
+tk_window.mainloop()
+
+# -
